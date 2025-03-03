5b867752cf02de88938af5116f62245fad088139
Nathan Smith <nathanielvs@gmail.com>
2025-03-03 07:24:56 -0700

    Initial commit

---
+++ kindwords.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: KindWords - Testimonials Plugin
+ * Description: A simple plugin to register a "kindwords" custom post type and display testimonials via shortcode.
+ * Version: 1.0
+ * Author: Nathan Smith
+ * License: GPL2+
+ * Text Domain: kindwords
+ */
